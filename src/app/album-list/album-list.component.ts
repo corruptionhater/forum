84a83b30c8de5b005c8a25237a752c3f2ad76cd5
Napoleon Julius von Bismark <icaunus@outlook.com>
2017-11-22 11:54:36 +0200

    components generated

---
+++ src/app/album-list/album-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'album-list',
+  templateUrl: './album-list.component.html',
+  styleUrls: ['./album-list.component.css']
+})
+export class AlbumListComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AlbumListComponent } from './album-list/album-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    UserListComponent,
+    AlbumListComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +19,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
