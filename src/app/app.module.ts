2dd5b2ad8ec71a354f734235f66886f4aea0e63d
Napoleon Julius von Bismark <icaunus@outlook.com>
2017-11-23 03:02:01 +0200

    maybe final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
-import { UserListComponent } from './user-list/user-list.component';
+import { UserTableComponent } from './user-table/user-table.component';
+import { UserTableService } from './user-table/user-table.service';
 import { AlbumListComponent } from './album-list/album-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    UserListComponent,
+    UserTableComponent,
     AlbumListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserTableService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
